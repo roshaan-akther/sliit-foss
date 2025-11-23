ccf2ae27a83a3550c4cccb4ba107059d357f9ac7
VoidNet <roshaanakther.nrf@gmail.com>
2025-11-23 13:25:36 +0530

    Create submissions/ directory and move api phantom submission zip
    - Added submissions/ folder in repository root for organizing challenge submissions
    - Moved submission.zip (api phantom) into submissions/
    - Structure: submissions/submission.zip contains submission/execute.sh at root level
    - Ready for organized one-by-one challenge submissions

--- api phantom/submission/execute.sh
+++ api phantom/submission/execute.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # get bitcoin price from coingecko
-# api might be slow sometimes
-# TODO: maybe cache the result or something
 curl -s "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd" | jq -r '.bitcoin.usd'
